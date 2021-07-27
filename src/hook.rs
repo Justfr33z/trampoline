aee4139bfd742b12d43bbd7c5cbf21b4ac9c7311
JustFr33z <justfr33z@mailbox.org>
2021-07-27 19:10:03 +0200

    Add hook function declarations

--- src/hook.rs
+++ src/hook.rs
@@ -1,3 +1,20 @@
+use crate::Result;
+use std::ffi::c_void;
+
 pub struct Hook;
 
 pub struct TrampolineHook;
+
+impl Hook {
+    pub fn hook(src: *mut c_void, dst: *const c_void, len: usize) -> Result<Self> {
+        todo!()
+    }
+
+    pub fn unhook(&self) -> Result<()> {
+        todo!()
+    }
+
+    pub fn active(&self) -> bool {
+        todo!()
+    }
+}
