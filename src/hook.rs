bcb6630367e375855829fd333a52ce68e389c5bc
JustFr33z <justfr33z@mailbox.org>
2021-07-28 22:27:49 +0200

    Update hook and trampoline fields

--- src/hook.rs
+++ src/hook.rs
@@ -1,9 +1,17 @@
 use crate::Result;
 use std::ffi::c_void;
 
-pub struct Hook;
+pub struct Hook {
+    src: *mut c_void,
+    len: usize,
+    orig_bytes: Vec<u8>,
+    active: bool,
+}
 
-pub struct TrampolineHook;
+pub struct TrampolineHook {
+    gateway: *mut c_void,
+    hook: Hook,
+}
 
 impl Hook {
     pub fn hook(src: *mut c_void, dst: *mut c_void, len: usize) -> Result<Self> {
